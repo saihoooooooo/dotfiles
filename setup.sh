db6c167d32d2132a93f4da2fc948c404275e2985
saihoooooooo <saihoooooooo@gmail.com>
2013-08-20 11:04:24 +0900

    Added setup.sh.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR=$(cd $(dirname $0); pwd)
+
+for FILE in `ls -a`; do
+    if [ $FILE != "." ] && [ $FILE != ".." ] && [ $FILE != ".git" ] && [ $FILE != "setup.sh" ]; then
+        ln -s $DIR/$FILE ../$FILE
+    fi
+done
+
+mkdir -p ../.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ../.vim/bundle/neobundle.vim
