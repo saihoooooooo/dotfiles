03b35b7cd2a0b480afce999757d9b055fded565e
saihoooooooo <saihoooooooo@gmail.com>
2013-08-21 17:21:16 +0900

    seiriseiton.

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ DIR=$(cd $(dirname $0); pwd)
 
 for FILE in `ls -a`; do
     if [ $FILE != "." ] && [ $FILE != ".." ] && [ $FILE != ".git" ] && [ $FILE != "setup.sh" ]; then
-        ln -s $DIR/$FILE ../$FILE
+        ln -s $DIR/$FILE ..
     fi
 done
 
 mkdir -p ../.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ../.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/neobundle.vim $_
